a6d6320e9b23ac620303f5fe7b0a7c62d2fd0d7f
yuanbo@mit.edu <yuanbo@mit.edu>
2020-09-10 16:02:34 -0400

    1

---
+++ pacmen/start.js
@@ -0,0 +1,35 @@
+var pos = 0;
+const pacArray = [
+    ['PacMan1.png', 'PacMan2.png'],
+    ['PacMan3.png', 'PacMan4.png']
+];
+var direction = 0;
+var focus = 0;
+
+function Run() {
+    let img = document.getElementById("PacMan");
+    let imgWidth = img.width
+    focus = (focus + 1) % 2;
+    direction = checkPageBounds(direction, imgWidth);
+    img.src = pacArray[direction][focus];
+    if (direction) {
+        pos -= 20;
+        img.style.left = pos + "px";
+    } else {
+        pos += 20;
+        img.style.left = pos + 'px'; //changing the position. 
+    }
+    setTimeout(Run, 200);
+    // Use setTimeout to call Run every 200 millesecs
+}
+
+function checkPageBounds(direction, imgWidth) {
+    //
+    // Complete this to reverse direction on hitting page bounds
+    //
+    if pos == window.innerWidth - imgWidth {
+        direction = 1;
+    }
+    else if pos == 0 {direction = 0;} 
+    return direction;
+}
