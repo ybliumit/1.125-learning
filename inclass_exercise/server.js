c79f1507ecd0f54b9725d3e413b1b952ffde49b5
ybliumit <yuanbo@mit.edu>
2020-10-08 15:01:27 -0400

    1

--- inclass_exercise/server.js
+++ inclass_exercise/server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res){
+    res.send('hello!');
+})
+
 app.use(express.static('myfiles'));
 
-app.listen(8080, function(){
-    console.log('ready at 8080');
+app.listen(3000, function(){
+    console.log('running on port 3000');
 })
