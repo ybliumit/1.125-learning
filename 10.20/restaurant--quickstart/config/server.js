ac9ef8ac06151baa22d5b8852a06a11d166ced93
ybliumit <yuanbo@mit.edu>
2020-10-22 15:21:54 -0400

    1

---
+++ 10.20/restaurant--quickstart/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '505aacfcae16ac999315cc681969a8a0'),
+    },
+  },
+});
---
+++ 10.20/restaurant--quickstart/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f5fd7730-7c4e-4952-9a94-aed8da61faee'
+};
